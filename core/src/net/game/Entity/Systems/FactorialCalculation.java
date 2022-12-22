6eebb94c30a7d4d2cf3d381dcb36415300b659ad
stand <standrik1701@gmail.com>
2022-12-22 10:56:38 +0300

    Recursion and interfaces in FactorialCalculation added

---
+++ core/src/net/game/Entity/Systems/FactorialCalculation.java
@@ -0,0 +1,17 @@
+package net.game.Entity.Systems;
+
+public class FactorialCalculation implements GetRes{
+    private int res;
+    FactorialCalculation(int n){
+        res = calculate(n);
+    }
+    int calculate(int n){
+        if(n == 1) return 1;
+        else return n*(n-1);
+    }
+
+    @Override
+    public int getResult() {
+        return this.res;
+    }
+}
